8b73adc0711fca4b4f1e138d31a65825b82616df
Rob Aird <robkaird@gmail.com>
2017-11-16 19:05:55 -0600

    (feat) getCoordinates call to Google API [searchUtils.js]

--- utils/searchUtils.js
+++ utils/searchUtils.js
@@ -1,3 +1,5 @@
+const request = require('request-promise');
+
 module.exports = {
   nearbyZips(lat, lng, dist, callback, connection) {
     connection.query(`SELECT postalCode, ( 3959 * acos( cos( radians(${lat}) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(${lng}) ) + sin( radians(${lat}) ) * sin( radians( lat ) ) ) ) AS distance FROM postalcodes HAVING (distance < ${dist}) ORDER BY distance;`, (err, rows) => {
@@ -5,5 +7,9 @@ module.exports = {
       const zipString = zips.join("%' OR address LIKE '%");
       callback(zipString);
     });
+  },
+
+  getCoords(location) {
+    return request(`https://maps.googleapis.com/maps/api/geocode/json?${location}`);
   },
 };
