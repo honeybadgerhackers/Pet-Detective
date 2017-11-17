d6d689364d32d28a9551dbed1f1369f87a9bf4b8
Rob Aird <robkaird@gmail.com>
2017-11-16 21:58:51 -0600

    (refactor) removed zip search radius function [searchUtils.js] `# Please enter the commit message for your changes. Lines starting

--- utils/searchUtils.js
+++ utils/searchUtils.js
@@ -1,24 +1,12 @@
 const request = require('request-promise');
 
 module.exports = {
-  nearbyZips(lat, lng, dist, callback, connection) {
-    connection.query(`SELECT postalCode, ( 3959 * acos( cos( radians(${lat}) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(${lng}) ) + sin( radians(${lat}) ) * sin( radians( lat ) ) ) ) AS distance FROM postalcodes HAVING (distance < ${dist}) ORDER BY distance;`, (err, rows) => {
-      const zips = rows.map(row => row.postalCode);
-      const zipString = zips.join("%' OR address LIKE '%");
-      callback(zipString);
-    });
-  },
-
   getCoords(location, GOOGLE_API_KEY) {
     location = location.split(' ').join('+');
     return request(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${GOOGLE_API_KEY}`);
   },
 
   radiusSearch(lat, lng, dist, callback, connection) {
-    if (!dist) {
-      dist = 0;
-    }
-    console.log(dist);
     connection.query(`SELECT *, ( 3959 * acos( cos( radians(${lat}) ) * cos( radians( SUBSTRING_INDEX(latlong, ',', 1) ) ) * cos( radians( SUBSTRING_INDEX(latlong, ',', -1) ) - radians(${lng}) ) + sin( radians(${lat}) ) * sin( radians( SUBSTRING_INDEX(latlong, ',', 1) ) ) ) ) AS distance FROM petpost HAVING (distance < ${dist}) ORDER BY distance;`, (err, rows) => {
       if (err) {
         callback(err);
