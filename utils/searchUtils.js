fda8f302d6e782073c12b87227bee28fa4844b69
Rob Aird <robkaird@gmail.com>
2017-11-16 19:46:38 -0600

    (feat) radiusSearch that gets petpost

--- utils/searchUtils.js
+++ utils/searchUtils.js
@@ -13,4 +13,12 @@ module.exports = {
     location = location.split(' ').join('+');
     return request(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${GOOGLE_API_KEY}`);
   },
+
+  radiusSearch(lat, lng, dist, callback, connection) {
+    connection.query(`SELECT *, ( 3959 * acos( cos( radians(${lat}) ) * cos( radians( SUBSTRING_INDEX(latlong, ',', 1) ) ) * cos( radians( SUBSTRING_INDEX(latlong, ',', -1) ) - radians(${lng}) ) + sin( radians(${lat}) ) * sin( radians( SUBSTRING_INDEX(latlong, ',', 1) ) ) ) ) AS distance FROM petposts HAVING (distance < ${dist}) ORDER BY distance;`, (err, rows) => {
+      const zips = rows.map(row => row.postalCode);
+      const zipString = zips.join("%' OR address LIKE '%");
+      callback(zipString);
+    });
+  };
 };
