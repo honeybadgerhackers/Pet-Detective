7c479f1b1388358784cb1ddf3ef465f882f2cc49
Rob Aird <robkaird@gmail.com>
2017-11-19 22:06:11 -0600

    (feat) pages when more than 10 bulletins [petForm.html, app.js, index.html]

--- client/components/app.js
+++ client/components/app.js
@@ -1,4 +1,4 @@
-angular.module('pet-detective', ['google.places', 'ngTagsInput'])
+angular.module('pet-detective', ['google.places', 'ngTagsInput', 'angularUtils.directives.dirPagination'])
   .controller('appController', function () {
   })
   .directive('app', function () {
